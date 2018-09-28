f13ae9b534d1df70c8cc06c95fde3a10c46e1361
Guilherme Siqueira <guilhersiqueira@gmail.com>
2018-09-28 00:32:00 -0300

    Adding tabs with cards

---
+++ app/components/tabs.js
@@ -0,0 +1,20 @@
+var tabButtons=document.querySelectorAll(".tabs .button button");
+var tabPanels=document.querySelectorAll(".tabs  .tab");
+var tabPanels2=document.querySelectorAll(".tabs  .tab2");
+
+function showPanel(panelIndex) {
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    switch(panelIndex){
+        case 0:
+            tabPanels[panelIndex].style.display="block";
+            tabPanels2[panelIndex].style.display="none";
+        break;
+        case 1:
+            tabPanels[panelIndex-1].style.display="none";
+            tabPanels2[panelIndex-1].style.display="block";
+        break;
+    }
+}
+showPanel(0);
