780f25dd79c6eb103555c1c8936d3fccea74a5fb
lucasandrad <lucasandrad@yandex.com>
2018-10-07 20:57:30 -0300

    Finish refactor with tabs and table

--- app/components/tabs.js
+++ app/components/tabs.js
@@ -1,15 +1,13 @@
-
 var tab1 = document.getElementById('tab1');
 var tab2 = document.getElementById('tab2');
 
-function toggleTabs(tabId){
-    console.log(tab1.style);
-    if(tabId<2){
-        tab1.style.display = 'inherit';
-        tab2.style.display = 'none';
-    }
-    else{
-        tab1.style.display = 'none';
-        tab2.style.display = 'inherit';
-    }
+function toggleTabs(tabId) {
+	console.log(tab1.style);
+	if (tabId < 2) {
+		tab1.style.display = 'inherit';
+		tab2.style.display = 'none';
+	} else {
+		tab1.style.display = 'none';
+		tab2.style.display = 'inherit';
+	}
 }
