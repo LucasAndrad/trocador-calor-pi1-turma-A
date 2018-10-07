abad9cf7eebecb995b06b711817f14985b3bf5db
lucasandrad <lucasandrad@yandex.com>
2018-10-07 17:18:04 -0300

    Pass table to new tabs

--- app/components/tabs-a.js
+++ app/components/tabs-a.js
@@ -1,13 +1,15 @@
 
-function toggleTabs(tabId){
 var tab1a = document.getElementById('tab-a1');
 var tab2a = document.getElementById('tab-a2');
+
+function toggleTabs(tabId){
+    console.log(tab1a.style);
     if(tabId<2){
-        tab1a.style.display('inherit');
-        tab2a.style.display('none');
+        tab1a.style.display = 'inherit';
+        tab2a.style.display = 'none';
     }
     else{
-        tab1a.style.display('none');
-        tab2a.style.display('inherit');
+        tab1a.style.display = 'none';
+        tab2a.style.display = 'inherit';
     }
 }
