69ffe2102d2c070331e2241385220e0890e0de0d
lucasandrad <lucasandrad@yandex.com>
2018-10-07 22:15:51 -0300

    Merge with feature/sensordData

--- app/components/tabs.js
+++ app/components/tabs.js
@@ -2,7 +2,6 @@ var tab1 = document.getElementById('tab1');
 var tab2 = document.getElementById('tab2');
 
 function toggleTabs(tabId) {
-	console.log(tab1.style);
 	if (tabId < 2) {
 		tab1.style.display = 'inherit';
 		tab2.style.display = 'none';
