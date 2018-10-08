ab4fa110b41d1997f51f66b711618f31b7177879
lucasandrad <lucasandrad@yandex.com>
2018-10-07 23:56:54 -0300

    Finish tabs refactor. Change all the style when user hover or click it

--- app/components/tabs.js
+++ app/components/tabs.js
@@ -1,12 +1,21 @@
+var tabs = document.getElementById('tabs');
 var tab1 = document.getElementById('tab1');
 var tab2 = document.getElementById('tab2');
+var tab1Title =  document.getElementById('tab1-title');
+var tab2Title =  document.getElementById('tab2-title');
 
 function toggleTabs(tabId) {
 	if (tabId < 2) {
 		tab1.style.display = 'inherit';
 		tab2.style.display = 'none';
+		tab1Title.style.border = '2px solid #769bff';
+		tab2Title.style.border = '2px solid #ffffff';
+		tabs.style.borderTop = '10px solid #769bff';
 	} else {
 		tab1.style.display = 'none';
 		tab2.style.display = 'inherit';
+		tab1Title.style.border = '2px solid #ffffff';
+		tab2Title.style.border = '2px solid #fd5f54';
+		tabs.style.borderTop = '10px solid #fd5f54';
 	}
 }
