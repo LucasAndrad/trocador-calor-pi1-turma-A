45e50fa3674e7454231200cb888cb28cece8aca4
Guilherme Siqueira <guilhersiqueira@gmail.com>
2018-10-12 13:16:42 -0300

    Adding inputfields for simulation

---
+++ app/components/inputs.js
@@ -0,0 +1,15 @@
+
+function receivesInput(tc1, tc2){
+    var form = document.getElementById("form1");
+    var tc1 = document.getElementById("tc1").value;
+    var tc2 = document.getElementById("tc2").value;        
+    console.log("TC1 = " + tc1);
+    console.log("TC2 = " + tc2);
+    // form.addEventListener('click', function(event) {
+    //     if(tc1 == '1' || tc2 === '1'){
+    //         event.preventDefault();
+    //         console.log("Não enviado");
+    //     }
+    // });
+
+}
