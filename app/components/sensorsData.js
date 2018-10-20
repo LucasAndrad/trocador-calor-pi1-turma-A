ca14ada114eed44a03ac673ea0ad2e63774d9e29
Andre Lucas <andrelucax@gmail.com>
2018-10-20 17:59:37 -0300

    Added parse to define how input should be treated

--- app/components/sensorsData.js
+++ app/components/sensorsData.js
@@ -3,17 +3,14 @@
 // https://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript
 
   var SerialPort = require('serialport');
+  var Readline = SerialPort.parsers.Readline
   var port = new SerialPort('/dev/ttyACM0',{
     baudRate: 9600,
   });
 
-  port.on('open', onOpen);
-  port.on('data', onData);
+  var parser = new Readline()
+  port.pipe(parser)
 
-  function onOpen(){
-    console.log('Open Conection')
-  }
-
-  function onData(data){
-    console.log(data.toString());
-  }
+  parser.on('data', function (data) {
+    console.log(data)
+  })
