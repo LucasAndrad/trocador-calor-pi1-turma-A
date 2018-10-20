8522866cb850a64c628a74f12e87c3c82492954f
Andre Lucas <andrelucax@gmail.com>
2018-10-20 18:10:06 -0300

    Added default values to sensors.

--- app/components/sensorsData.js
+++ app/components/sensorsData.js
@@ -3,6 +3,11 @@
 // https://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript
 
 function getSensorsDatas(){
+  document.getElementById("s-temperature1").innerHTML = 0;
+  document.getElementById("s-temperature2").innerHTML = 0;
+  document.getElementById("s-temperature3").innerHTML = 0;
+  document.getElementById("s-temperature4").innerHTML = 0;
+
   var SerialPort = require('serialport');
   var Readline = SerialPort.parsers.Readline
   var port = new SerialPort('/dev/ttyACM0',{
