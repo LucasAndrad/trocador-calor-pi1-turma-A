1ef7c15fdf3187119ebc5542f36c8e93232886e7
Andre Lucas <andrelucax@gmail.com>
2018-10-21 00:43:16 -0300

    Fixing bug where unwanted text was showing up

--- app/components/sensorsData.js
+++ app/components/sensorsData.js
@@ -18,41 +18,32 @@ function getSensorsDatas(){
   port.pipe(parser)
 
   var lastData;
-  var dataAmnt = 0;
 
   parser.on('data', function (data) {
-    // Trying to discord data with bad params (First datas tends to have bad params)
-    if (dataAmnt < 10){
-      dataAmnt++;
-    }
+    var parsed_data = parseFloat(data);
 
-    // Trying to get data with good paraams
-    else{
-      var parsed_data = parseFloat(data);
-
-      // If data is float
-      if (parsed_data != NaN){
-        if (lastData == "D7"){
-          document.getElementById("s-temperature1").innerHTML = data;
-        }
-        else if (lastData == "F5"){
-          document.getElementById("s-temperature2").innerHTML = data;
-        }
-        else if (lastData == "C3"){
-          document.getElementById("s-temperature3").innerHTML = data;
-        }
-        else if (lastData == "9C"){
-          document.getElementById("s-temperature4").innerHTML = data;
-        }
-        else if (lastData == "9E"){
-          document.getElementById("s-temperature5").innerHTML = data;
-        }
+    // If data is float
+    if (parsed_data != NaN && data.length < 10){
+      if (lastData == "D7"){
+        document.getElementById("s-temperature1").innerHTML = data;
+      }
+      else if (lastData == "F5"){
+        document.getElementById("s-temperature2").innerHTML = data;
+      }
+      else if (lastData == "C3"){
+        document.getElementById("s-temperature3").innerHTML = data;
+      }
+      else if (lastData == "9C"){
+        document.getElementById("s-temperature4").innerHTML = data;
+      }
+      else if (lastData == "9E"){
+        document.getElementById("s-temperature5").innerHTML = data;
       }
     }
 
     // Trying to get last 2 digits of data
     if(data.length >= 3){
-      lastData = data[data.length - 3]+data[data.length - 2];
+      lastData = data[data.length - 3] + data[data.length - 2];
     }
   })
 }
