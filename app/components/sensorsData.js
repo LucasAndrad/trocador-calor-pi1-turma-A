43ef7797ffd472db10beac1a4dec35da4b4e2ae7
Andre Lucas <andrelucax@gmail.com>
2018-10-21 00:50:17 -0300

    Added comments. Fixed last sensor number dot

--- app/components/sensorsData.js
+++ app/components/sensorsData.js
@@ -11,18 +11,23 @@ function getSensorsDatas(){
   var Readline = SerialPort.parsers.Readline
   // '/dev/ttyACM0' is the port for arduino
   var port = new SerialPort('/dev/ttyACM0',{
+    // Same rate as arduino
     baudRate: 9600,
   });
 
+  // Adding parse so it gets full line instead of parts
   var parser = new Readline()
   port.pipe(parser)
 
+  // Aux variable to know if last data was serial number or temperature
   var lastData;
 
+  // Getting data from serial port
   parser.on('data', function (data) {
     var parsed_data = parseFloat(data);
 
     // If data is float
+    // Data.legth is used because sometimes it was getting unwanted values from serial number
     if (parsed_data != NaN && data.length < 10){
       if (lastData == "D7"){
         document.getElementById("s-temperature1").innerHTML = data;
