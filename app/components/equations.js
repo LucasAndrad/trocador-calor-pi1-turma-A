d7234692bc6620bca3a7b7c5d6beba22397e55d7
Guilherme Siqueira <guilhersiqueira@gmail.com>
2018-10-22 11:34:57 -0300

    Update equations.js

--- app/components/equations.js
+++ app/components/equations.js
@@ -1,31 +1,18 @@
-var tc1 = document.getElementById("tc1").value;
-var tc2 = document.getElementById("tc2").value;     
-var mc = document.getElementById("mc").value;    
-var th1 = document.getElementById("th1").value;        
-var mh = document.getElementById("mh").value;    
-var cph = document.getElementById("cph").value;    
-var cpc = document.getElementById("cpc").value;
-var hotWaterTemperature = document.getElementById('finalHotWaterTemperature');
-
-function showsInput(){
-    console.log("TC1 = " + tc1);
-    console.log("TC2 = " + tc2);
-    console.log("mc = " + mc);
-    console.log("th1 = " + th1);
-    console.log("mh = " + mh);
-    console.log("cph = " + cph);
-    console.log("cpc = " + cpc);
-}
-
-function heatTransferRate(){
+function heatTransferRate(mc, cpc, tc2, tc1){
     qc = mc * cpc * (tc2 - tc1);
-    // console.log("qc = " + qc);
     return qc;
 }
 
 function finalHotWaterTemperature(){
-    qc = heatTransferRate();
-    showsInput();
+    var tc1 = document.getElementById("tc1").value;
+    var tc2 = document.getElementById("tc2").value;     
+    var mc = document.getElementById("mc").value;    
+    var th1 = document.getElementById("th1").value;        
+    var mh = document.getElementById("mh").value;    
+    var cph = document.getElementById("cph").value;    
+    var cpc = document.getElementById("cpc").value;
+    var hotWaterTemperature = document.getElementById('finalHotWaterTemperature');
+    qc = heatTransferRate(mc, cpc, tc2, tc1);
     console.log("qc = " + qc);
     temp = ((qc/(mh*cph)));
     th2 = th1 - temp;
