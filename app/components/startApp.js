36366155d66fb5304d18f7ef4d3da4b6dbb41285
Andre Lucas <andrelucax@gmail.com>
2018-10-22 10:26:39 -0300

    Created initializer function to define default values and build tabble.

---
+++ app/components/startApp.js
@@ -0,0 +1,10 @@
+function startApp(){
+  buildTable();
+
+  // Default values for sensors temperatures
+  document.getElementById("s-temperature1").innerHTML = 0;
+  document.getElementById("s-temperature2").innerHTML = 0;
+  document.getElementById("s-temperature3").innerHTML = 0;
+  document.getElementById("s-temperature4").innerHTML = 0;
+  document.getElementById("s-temperature5").innerHTML = 0;
+}
