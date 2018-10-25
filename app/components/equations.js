ed29a31506005fc4f75729c3be95c5f18a2368f4
Guilherme Siqueira <guilhersiqueira@gmail.com>
2018-10-25 13:52:54 -0300

    Update equation heatTransferRate

--- app/components/equations.js
+++ app/components/equations.js
@@ -1,5 +1,6 @@
-function heatTransferRate(mc, cpc, tc2, tc1){
+function heatTransferRate(mc, tc2, tc1, cpc){
     qc = mc * cpc * (tc2 - tc1);
+    console.log("cpc = " + cpc)
     return qc;
 }
 
@@ -10,9 +11,10 @@ function finalHotWaterTemperature(){
     var th1 = document.getElementById("th1").value;        
     var mh = document.getElementById("mh").value;    
     var cph = document.getElementById("cph").value;    
-    var cpc = document.getElementById("cpc").value;
+    var cpc = 0;
     var hotWaterTemperature = document.getElementById('finalHotWaterTemperature');
-    qc = heatTransferRate(mc, cpc, tc2, tc1);
+    cpc = ((parseInt(tc1) + parseInt(tc2))/2);
+    qc = heatTransferRate(mc,tc2,tc1,cpc);
     temp = ((qc/(mh*cph)));
     th2 = th1 - temp;
     var arredondado = parseFloat(th2.toFixed(4));
