41a88f0d44ede65ace6e48b4e61fae5f6db40882
gabrielademoraes <gabrielademoraes10@gmail.com>
2018-10-26 19:47:41 -0200

    Criando função para ler o json e acrescentando botão de informação.

--- app/components/equations.js
+++ app/components/equations.js
@@ -1,12 +1,12 @@
 
 function Interpolizer(){
     var a, b, c, d, e;
-    a = 22;
-    b = 27;
-    c =  4.179;
-    d = 4.181;
-    e = 25;
-    
+    a = 22; // A é limite inferior
+    b = 27; //B é limite superior
+    c =  4.179; //C é
+    d = 4.181; //D é
+    e = 25; //E é o que quer saber (Entrada)
+
     x = d + (((e-a) * (c-d)) / (b-a))
     console.log("Interpolação = " + x);
 }
@@ -14,16 +14,24 @@ function Interpolizer(){
 function heatTransferRate(mc, tc2, tc1, cpc){
     qc = mc * cpc * (tc2 - tc1);
     console.log("cpc = " + cpc)
+    readTable();
     return qc;
 }
 
+function readTable(){
+  for(i = 0; i < tableOfTPSW.length; i++){
+    console.log(tableOfTPSW[i].temperature, tableOfTPSW[i].cpf);
+  }
+}
+
+
 function finalHotWaterTemperature(){
     var tc1 = document.getElementById("tc1").value;
-    var tc2 = document.getElementById("tc2").value;     
-    var mc = document.getElementById("mc").value;    
-    var th1 = document.getElementById("th1").value;        
-    var mh = document.getElementById("mh").value;    
-    var cph = document.getElementById("cph").value;    
+    var tc2 = document.getElementById("tc2").value;
+    var mc = document.getElementById("mc").value;
+    var th1 = document.getElementById("th1").value;
+    var mh = document.getElementById("mh").value;
+    var cph = document.getElementById("cph").value;
     var cpc = 0;
     Interpolizer();
     var hotWaterTemperature = document.getElementById('finalHotWaterTemperature');
