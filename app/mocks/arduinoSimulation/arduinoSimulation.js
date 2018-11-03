477449f91ac20743a86adb1c5f95a9b5a055bf36
Andre Lucas <andrelucax@gmail.com>
2018-11-02 22:08:23 -0300

    Addpting simulation to simulate new data incoming.

--- app/mocks/arduinoSimulation/arduinoSimulation.js
+++ app/mocks/arduinoSimulation/arduinoSimulation.js
@@ -10,37 +10,63 @@ async function arduino_simulation(){
       case 0:
         await sleep(250);
         port.write('28 AA AA 3E 13 13 2 F5 \n');
+        await sleep(50);
         temperature = generateRandomNumber(20 , 25);
-        await sleep(250);
         port.write(temperature);
+        await sleep(50);
+        flux = generateRandomNumber(1 , 5);
+        port.write(flux);
+        await sleep(50);
+        flux = generateRandomNumber(1 , 5);
+        port.write(flux);
         break;
       case 1:
         await sleep(250);
         port.write('28 AA 81 EE 12 13 2 D7 \n');
         temperature = generateRandomNumber(30 , 35);
-        await sleep(250);
         port.write(temperature);
+        await sleep(50);
+        flux = generateRandomNumber(1 , 5);
+        port.write(flux);
+        await sleep(50);
+        flux = generateRandomNumber(1 , 5);
+        port.write(flux);
         break;
       case 2:
         await sleep(250);
         port.write('28 59 DD 92 1B 13 1 9E \n');
         temperature = generateRandomNumber(40 , 45);
-        await sleep(250);
         port.write(temperature);
+        await sleep(50);
+        flux = generateRandomNumber(1 , 5);
+        port.write(flux);
+        await sleep(50);
+        flux = generateRandomNumber(1 , 5);
+        port.write(flux);
         break;
       case 3:
         await sleep(250);
         port.write('28 D C7 84 1B 13 1 9C \n');
         temperature = generateRandomNumber(58 , 62);
-        await sleep(250);
         port.write(temperature);
+        await sleep(50);
+        flux = generateRandomNumber(1 , 5);
+        port.write(flux);
+        await sleep(50);
+        flux = generateRandomNumber(1 , 5);
+        port.write(flux);
         break;
       case 4:
         await sleep(250);
         port.write('28 2F 99 97 1B 13 1 C3 \n');
         temperature = generateRandomNumber(60 , 65);
-        await sleep(250);
         port.write(temperature);
+        await sleep(50);
+        flux = generateRandomNumber(1 , 5);
+        port.write(flux);
+        await sleep(50);
+        flux = generateRandomNumber(1 , 5);
+        port.write(flux);
         break;
     }
   }
