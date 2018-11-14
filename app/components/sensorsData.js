c31931ded521c926c3f824b82b58ac19f636f934
lucasandrad <lucasandrad@yandex.com>
2018-11-14 00:06:35 -0200

    Add first structure for calc on tab1

--- app/components/sensorsData.js
+++ app/components/sensorsData.js
@@ -1,13 +1,19 @@
-const {dialog} = require('electron').remote
+const {
+  dialog
+} = require('electron').remote
 
 // Making communication with Serial Port
 var SerialPort = require('serialport');
 var Readline = SerialPort.parsers.Readline
 // '/dev/ttyACM0' is the port for arduino
-var path = '/tmp/ttyV0' // '/tmp/ttyV0' for data simulation
-var port = new SerialPort(path, { autoOpen: false, baudRate: 9600 });
+var path = '/dev/ttyACM0' // '/tmp/ttyV0' for data simulation
+var port = new SerialPort(path, {
+  autoOpen: false,
+  baudRate: 9600
+});
 
 function stopSensorsDatas() {
+  port.write('CLOSE');
   port.close();
 }
 
@@ -15,7 +21,7 @@ function startButtonHolder() {
   return 0;
 }
 
-var nextSensorData = [0,0,0,0,0]
+var nextSensorData = [0, 0, 0, 0, 0]
 
 async function getSensorsDatas() {
   port.open(function (err) {
@@ -24,26 +30,36 @@ async function getSensorsDatas() {
     }
   });
   // If it starts communication desactivate button
-  port.on('open', function(err) {
-    document.getElementById('start-sensors-button').setAttribute( "onclick", "startButtonHolder()" );
+  port.on('open', function (err) {
+    document.getElementById('start-sensors-button').setAttribute("onclick", "startButtonHolder()");
   });
 
   // Sleep 200ms to get real result of port.isOpen
   await sleep(200);
 
   // Give alert if fail to connect
-  if(!port.isOpen){
-    const dialogOptions = {title: 'Erro de comunicação', type: 'info', buttons: ['OK'], message: 'Não foi possível realizar a conexão com o sensor.\nVerifique se o mesmo está conectado e tente novamente.'}
+  if (!port.isOpen) {
+    const dialogOptions = {
+      title: 'Erro de comunicação',
+      type: 'info',
+      buttons: ['OK'],
+      message: 'Não foi possível realizar a conexão com o sensor.\nVerifique se o mesmo está conectado e tente novamente.'
+    }
     dialog.showMessageBox(dialogOptions)
   }
 
   var alreadyClosed = false;
   port.on('close', function (err) {
-    if (!alreadyClosed){
+    if (!alreadyClosed) {
       // If communication breaks reactivate button
-      document.getElementById('start-sensors-button').setAttribute( "onclick", "getSensorsDatas()" );
+      document.getElementById('start-sensors-button').setAttribute("onclick", "getSensorsDatas()");
 
-      const dialogOptions = {title: 'Comunicação encerrada.', type: 'info', buttons: ['OK'], message: 'A comunicação foi encerrada.\n'}
+      const dialogOptions = {
+        title: 'Comunicação encerrada.',
+        type: 'info',
+        buttons: ['OK'],
+        message: 'A comunicação foi encerrada.\n'
+      }
       dialog.showMessageBox(dialogOptions)
     }
     alreadyClosed = true;
@@ -61,33 +77,29 @@ async function getSensorsDatas() {
     var parsed_data = parseFloat(data);
 
     // If data is numeric
-    if (typeof data != typeof NaN && data.length < 8){
-      if (lastData == "D7"){
+    if (typeof data != typeof NaN && data.length < 8) {
+      if (lastData == "D7") {
         document.getElementById("s-temperature1").innerHTML = data;
         nextSensorData[0] = parsed_data
         updateChart(nextSensorData)
-      }
-      else if (lastData == "F5"){
+      } else if (lastData == "F5") {
         document.getElementById("s-temperature2").innerHTML = data;
         nextSensorData[1] = parsed_data
-      }
-      else if (lastData == "C3"){
+      } else if (lastData == "C3") {
         document.getElementById("s-temperature3").innerHTML = data;
         nextSensorData[2] = parsed_data
-      }
-      else if (lastData == "9C"){
+      } else if (lastData == "9C") {
         checkStatus(data);
         document.getElementById("s-temperature4").innerHTML = data;
         nextSensorData[3] = parsed_data
-      }
-      else if (lastData == "9E"){
+      } else if (lastData == "9E") {
         document.getElementById("s-temperature5").innerHTML = data;
         nextSensorData[4] = parsed_data
       }
     }
 
     // Trying to get last 2 digits of data
-    if(data.length >= 3){
+    if (data.length >= 3) {
       lastData = data[data.length - 3] + data[data.length - 2];
     }
   })
