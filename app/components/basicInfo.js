342f4c45d5c37e1c6cb1c044287d9f28409c8eb2
lucasandrad <lucasandrad@yandex.com>
2018-11-15 11:12:16 -0200

    Add comments at basicInfo.js

--- app/components/basicInfo.js
+++ app/components/basicInfo.js
@@ -3,6 +3,12 @@ function updateBasicInfo(data) {
   tpdBasicInfo = tubesPressureDrop(mhBasicInfo);
   document.getElementById("basicInfoTubesPressure").innerHTML = tpdBasicInfo.toFixed(6);
 
+  // Perguntar quais são os repectivos sensores
+  // tc1 = data[0] = Temp agua fria de entrada
+  // tc2 = data[3] = Temp agua quente de saida
+  // th1 = data[1] = Temp agua quente de entrada
+  // th2Initial = 31.07567262 =  Temp agua quente de saida especulada
+
   hpdBasicInfo = hullPressureDrop(data[0], data[3], data[1], 31.07567262, mhBasicInfo);
   document.getElementById("basicInfoHullPressure").innerHTML = hpdBasicInfo.toFixed(6);
 }
